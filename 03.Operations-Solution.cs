4ed2190321e2f40d432bbe28c8fb53718ebab477
moher4o <samsoft@abv.bg>
2016-09-22 13:10:17 +0300

    Change in operations

--- 03.Operations-Solution.cs
+++ 03.Operations-Solution.cs
@@ -8,25 +8,6 @@ class Program
         int number2 = int.Parse(Console.ReadLine());
         string operations = Console.ReadLine();
 
-        if (operations == "+")
-        {
-            Console.Write($"{number1} + {number2} = {number1 + number2} - ");
-            Console.WriteLine((number1 + number2) % 2 == 0 ? "even" : "odd");
-        }
-        else if (operations == "-")   //тук съм правил аз промени
-        {
-            Console.Write($"{number1} + {number2} = {number1 + number2} - ");   //тук
-            Console.WriteLine((number1 + number2) % 2 == 0 ? "even" : "odd");   //и тук
-            Console.Write($"{number1} - {number2} = {number1 - number2} - ");
-            Console.WriteLine((number1 - number2) % 2 == 0 ? "even" : "odd");
-        }
-        else if (operations == "*")
-        {
-            Console.Write($"{number1} * {number2} = {number1 * number2} - ");
-            Console.WriteLine((number1 * number2) % 2 == 0 ? "even" : "odd");
-            Console.Write($"{number1} * {number2} = {number1 * number2} - ");
-            Console.WriteLine((number1 * number2) % 2 == 0 ? "even" : "odd");
-        }
         else if (operations == "/")
         {
             Console.WriteLine(number2 == 0
